4d125aede3f9b181afcaba715361e897bc76d217
smhrd <smhrd@210.223.207.38>
2021-09-06 17:13:23 +0900

    임포트테스트

---
+++ TestProject/src/main/java/com/smhrd/Import_test.java
@@ -0,0 +1,13 @@
+package com.smhrd;
+
+import java.util.Scanner;
+
+public class Import_test {
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("단어를 입력하세요 >> ");
+	String voca = sc.next();
+	System.out.println(voca);
+}
+}
