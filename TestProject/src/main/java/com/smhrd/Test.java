92b99d49ada9e735941450ea9989796d8ed9e1ae
smhrd <smhrd@210.223.207.38>
2021-09-06 17:07:30 +0900

    비번떠라

--- TestProject/src/main/java/com/smhrd/Test.java
+++ TestProject/src/main/java/com/smhrd/Test.java
@@ -9,7 +9,6 @@ public class Test {
 		System.out.println("내려내려 두번째 한글판!");
 		System.out.println("강휘가 올립니다 세번째!");
 		System.out.println("강휘가 올립니다 네번째!");
-		System.out.println("강휘가 올립니다 제발..!");
 	}
 
 }
