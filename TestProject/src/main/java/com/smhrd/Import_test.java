1fa7ba9d1476b3dd5d8fc206cf2d516fa28e9f21
smhrd <smhrd@210.223.207.38>
2021-09-06 17:18:27 +0900

    이강휘

--- TestProject/src/main/java/com/smhrd/Import_test.java
+++ TestProject/src/main/java/com/smhrd/Import_test.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("단어를 입력하세요 >> ");
 	String voca = sc.next();
+	System.out.println();
 	System.out.println(voca);
 }
 }
